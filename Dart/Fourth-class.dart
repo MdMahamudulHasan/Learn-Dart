4fc69c5a74221b700246c99f8b8a730ae50d83c1
MdMahamudulHasan <hmahamudul846@gmail.com>
2023-04-04 01:09:02 +0600

    String and Bool

--- Dart/Fourth-class.dart
+++ Dart/Fourth-class.dart
@@ -1,1 +1,43 @@
+void main() {
+  // print("Hello World");
+  // String name = "Rakib";
+  // String name2;
+  // name2 = "hasan";
+  // name2 = 'Mahmud';
 
+  // print(name2);
+
+  // String fname = "Kamrul";
+  // String lname = "Hasan";
+  // String Nname = "Rasel";
+
+  // print(fname);
+  // print(lname);
+  // print(Nname);
+  // print(fname + lname + Nname);
+  // print(fname + ' ' + lname + ' ' + Nname);
+  // print("$fname $lname $Nname");
+  // print("${fname} ${lname} ${Nname}");
+
+  // String fName = "Rakib";
+  // String Lname = "Mahmud";
+  // int age = 26;
+  // double gpa = 4.00;
+
+  // String info =
+  //     "First Name: ${fName}\nLast name: ${Lname}\nAge: ${age}\nGpa: ${gpa}";
+  // print(info);
+  // String username = age.toString();
+  // print(username);
+
+  // print('i\'m Kamrul Hasn');
+  // print("I'm Mahmud Hasan");
+
+  String name = "Raihan";
+  String name2 = "Rabia";
+  bool isMale = true;
+  bool isFemale = true;
+
+  print("Name : " + name + " \nMale : " + isMale.toString());
+  print("Name : " + name + "\nFeMale : " + isFemale.toString());
+}
